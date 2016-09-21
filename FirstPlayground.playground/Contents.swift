5ea49a27bf012bcd821fbf63ad3e0a93716b1bf7
snoupyrelete <drob8896@740s-pl-112.local>
2016-09-21 08:23:40 -0600

    Added plagrounds repo.

---
+++ FirstPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let listNames: [String] = ["Dylan","Michael", "Jeff"]
+
+
+
+var str = "Hello, playground"
+
+for name in listNames {
+    print(name + " 😁")
+}
